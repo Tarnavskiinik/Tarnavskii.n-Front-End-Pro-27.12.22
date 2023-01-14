2a4f42924b8af8fc9fc2f717d027f09d17c3a09a
unknown <nik.tarnavsky@gmail.com>
2023-01-14 12:56:14 +0200

    cikle

---
+++ index.js
@@ -0,0 +1,28 @@
+for (let num=20; num <= 30; num=num+0.5) {
+    console.log(num) 
+  }
+//
+for(var dol = 10; dol <= 100; dol+= 10) console.log(dol,dol * 27)
+
+//
+
+//
+let n = 10; 
+nextPrime:
+for (let i = 2; i <= n ; i++){
+    for(let j = 2; j < i; j++){
+    if(i % j ==0) continue nextPrime;
+}
+console.log(i);
+}
+//
+
+let number = prompt('Введите число , что бы проверить деление на 3: ')
+if(number % 3 === 0 && number){
+    alert('Число можно поделить на 3')
+}else{
+    alert('Число нельза поделить на 3');
+}
+
+
+
