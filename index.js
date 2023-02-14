220eee69fb923304859c7440c5770fac6fd03933
unknown <nik.tarnavsky@gmail.com>
2023-02-14 20:38:18 +0200

    end work

--- index.js
+++ index.js
@@ -1,24 +1,72 @@
+//  //1
+//    let arr = ['wm', 'h', 'hi', 1, 3,4 ,5, 7, 8, 'end'];
+//    function arithmeticMeane() {
+//     //  let numbers = arr.filter(item => {   
+//     //   return !isNaN(item) // typeof item === 'number'
+//     // });
+//     const numbers = arr.filter(item => 
+//       !isNaN(item) 
+//     );
 
+//      let sum = numbers.reduce((result, current) => result + current);
+//      return sum / numbers.length;
+//    }
+//    console.log(arithmeticMeane());
+//   //2
+
+//    function doMath(x, znak, y) {   
+//      if (~znak.search(/[^+\-*/%^]/) || znak.length !== 1) 
+//      return 'Ошибка';  
+//      return eval(x + znak.replace('^', '**') + y) } 
+//        const x = prompt('Введите x');
+//        const y = prompt('Введите y'); 
+//        const znak = prompt('Введите знак +, -, *, /, %, ^');
+//        alert(doMath(x, znak, y))
+
+//3
+//  function createArray(){
+//    const res = [];
+//    const len = +prompt('Введите');
+  
+//    for(let i =0; i < len; i++){
+//      const len2 = +prompt('Enter your');
+//      res[i] = []
+//      for(let j = 0; j < len2; j++){
+//        res[i][j] = prompt('enter num');
+//      }
+//    }
+  
+//    return res
+//  }
+
+//  console.log(createArray())
+//  function createArray(){
+//   const res = [];
+//   const len = +prompt('Введите');
+ 
+//   for(let i =0; i < len; i++){
+//     const len2 = +prompt('Enter your');
+//     res[i] = []
+//     for(let j = 0; j < len2; j++){
+//       res[i][j] = prompt('enter num');
+//     }
+//   }
+ 
+//   return res
+// }
+
+// console.log(createArray())
 //4
- function deleteSymbol() {
-   let text = prompt("Введите текст");
-   let symbol = prompt("Введите символы");
+function clearChar(str, chars){
+  for(const char of chars){
+    str = str.replace(RegExp(char, 'g'), '')
+  }
 
-   let textArr = text.split("");
-   let symbolArr = symbol.split("");
+  return str;
+}
 
-   for(let i = 0; i < textArr.length; i++) {
-       for(let j = 0; j < symbolArr.length; j++) {
-           if (textArr[i] === symbolArr[j]) {
-               textArr.splice(i, 1);
-               i--;
-             }
-         }
-   }
-     return textArr.join("");
- }
+const str = prompt('Enter your text');
+let chars = prompt('Enter your char. Separate ","');
+chars = chars.split(',');
 
- let functionResul = deleteSymbol();
- console.log(functionResul);
-
-
+console.log(clearChar(str, chars))
